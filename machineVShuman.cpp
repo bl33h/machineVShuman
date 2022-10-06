c7391efb5ca48599582c651115e80d604d8f28b0
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-10-06 07:19:29 -0600

    initial commit.

---
+++ machineVShuman.cpp
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------
+Copyright (C), 2022-2023, bl33h, MelissaPerez09, FabianJuarez182, SebasJuarez
+@author Sara Echeverria, Melissa Perez, Fabian Juarez, Sebastian Juarez
+FileName: machineVShuman
+@version: I
+Creation: 06/10/2022
+Last modification: 06/10/2022
+------------------------------------------------------------------------------*/
