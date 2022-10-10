90827208e6c2fc99a6c25f46d8ef52360b206d83
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-10-10 08:26:38 -0600

    bugs fixed.

--- src/machineVShuman.cpp
+++ src/machineVShuman.cpp
@@ -2,9 +2,9 @@
 Copyright (C), 2022-2023, bl33h, MelissaPerez09, FabianJuarez182, SebasJuarez
 @author Sara Echeverria, Melissa Perez, Fabian Juarez, Sebastian Juarez
 FileName: machineVShuman
-@version: II
+@version: III
 Creation: 06/10/2022
-Last modification: 06/10/2022
+Last modification: 10/10/2022
 ----------------------------------------------------------------------------------------------------
 * Descripción: 
 * Establecer la eficiencia y rentabilidad a largo plazo de la producción de una 
@@ -16,6 +16,8 @@ Last modification: 06/10/2022
 #include <pthread.h>
 #include <iostream>
 using namespace std;
+
+// Variables
 string tipoTrabajo;
 int cantProcesos;
 /* meter esta en el main por la cantidad de Personal int tiempoEjecucion[cantidadPersonal];*/
@@ -29,16 +31,21 @@ int ensamblajeMaquina;
 int piezasPMin;
 pthread_mutex_t mutex;
 
+// --- Estructura con la informacion de la empresa ---
 struct datosEmpresa
 {
     pthread_t thread;
     int contador;
 };
+
+// --- Estructura con la informacion del empleado ---
 struct datosEmpleado
 {
     pthread_t thread;
     int contador;
 };
+
+// --- Estructura con la informacion de los costos ---
 struct costo{
     pthread_t thread;
     int costoFijoEnergia;
@@ -47,9 +54,32 @@ struct costo{
     int costoVariable;  
 };
 
+// --- Metodo costos ---
+void* costoProduccion(void *arg)
+{
+    costo *costos;
+    costos = (costo*)arg;
+    int costoFijoTotal = (costos -> costoFijoEnergia) + (costos -> costoFijoAgua) + (costos-> costoFijoMateriaPrima);
+    int costoVariable = (cantPiezas/piezasPMin) * 2; // Valor fijo
+    int costoTotal = costoFijoTotal + costoVariable;
+}
+
+// --- Metodo produccion por parte del personal humano ---
+void* procesoPersonal(void *arg)
+{
+
+}
+
+// --- Metodo produccion por parte de la maquina ---
+void* procesoMaquina(void *arg)
+{
+       
+}
+
+// --- Main ---
 int main(){
     cout << "- B I E N V E N I D O -";
-    cout << "\nTipo de trabajo que va a realizar: ";
+    cout << "\nFinalidad de su empresa: ";
     cin >> tipoTrabajo;
     cout << "Cantidad de procesos a realizar: ";
     cin >> cantProcesos;
@@ -65,18 +95,5 @@ int main(){
     cin >> inversionMaquina;
     cout << "Presupuesto inicial: ";
     cin >> presupuesto;
-
-
     cout << "Inicializando los procesos...";
-}
-
-void* costoProduccion(void *arg){
-    costo *x;
-    x = (costo*)arg;
-    int costoFijoTotal = (x-> costoFijoEnergia) + (x -> costoFijoAgua) + (x-> costoFijoMateriaPrima);
-
-    int costoVariable = (cantPiezas/piezasPMin) * 2;
-
-    int costoTotal = costoFijoTotal + costoVariable;
-
 }
