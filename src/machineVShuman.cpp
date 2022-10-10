0576c5caf5a2850632918f1b5903f00726fc8c5d
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-10-10 14:09:08 -0600

    human production method.

--- src/machineVShuman.cpp
+++ src/machineVShuman.cpp
@@ -2,14 +2,16 @@
 Copyright (C), 2022-2023, bl33h, MelissaPerez09, FabianJuarez182, SebasJuarez
 @author Sara Echeverria, Melissa Perez, Fabian Juarez, Sebastian Juarez
 FileName: machineVShuman
-@version: III
+@version: IV
 Creation: 06/10/2022
 Last modification: 10/10/2022
 ----------------------------------------------------------------------------------------------------
-* Descripción: 
-* Establecer la eficiencia y rentabilidad a largo plazo de la producción de una 
-* máquina en comparación a la que el personal humano realizaría.
+* Descripcion: 
+* Establecer la eficiencia y rentabilidad a largo plazo de la produccion de una 
+* máquina en comparacion a la que el personal humano realizaria.
 -----------------------------------------------------------------*/
+
+// Librerias
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -19,17 +21,24 @@ using namespace std;
 
 // Variables
 string tipoTrabajo;
-int cantProcesos;
+//int cantProcesos;
 /* meter esta en el main por la cantidad de Personal int tiempoEjecucion[cantidadPersonal];*/
 int inversionMaquina;
 int salarioPersonal;
 int cantidadPersonal;
 int presupuesto;
 int proyeccion;
-int cantPiezas;
+//int cantPiezas;
 int ensamblajeMaquina;
 int piezasPMin;
 pthread_mutex_t mutex;
+
+// --- Estructura con la informacion de la produccion ---
+struct datosProduccion
+{
+    int cantProcesos = 0;
+    int cantPiezas = 0;
+};
 
 // --- Estructura con la informacion de la empresa ---
 struct datosEmpresa
@@ -65,15 +74,23 @@ void* costoProduccion(void *arg)
 }
 
 // --- Metodo produccion por parte del personal humano ---
-void* procesoPersonal(void *arg)
+void* procesoPersonal(void *piezas)
 {
-
+   datosProduccion *produccionStructure = (datosProduccion*) piezas; // *Structure call
+    // *Mutex lock*
+    pthread_mutex_lock(&lock);
+    cout<<"El proceso No."<< contador << ", realizado por el personal humano, ha producido 1 pieza." <<endl;
+    produccionStructure->cantPiezas += 1;
+    // *Mutex Unlock*
+    pthread_mutex_unlock(&lock);
+    pthread_mutex_destroy(&lock);
+    return NULL;
 }
 
 // --- Metodo produccion por parte de la maquina ---
 void* procesoMaquina(void *arg)
 {
-       
+
 }
 
 // --- Main ---
