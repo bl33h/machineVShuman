7dc67cfac9a5c95bb0de3a9295d66429aba056d5
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2022-10-10 20:54:10 -0600

    Finish lab 5

--- src/machineVShuman.cpp
+++ src/machineVShuman.cpp
@@ -2,7 +2,7 @@
 Copyright (C), 2022-2023, bl33h, MelissaPerez09, FabianJuarez182, SebasJuarez
 @author Sara Echeverria, Melissa Perez, Fabian Juarez, Sebastian Juarez
 FileName: machineVShuman
-@version: V
+@version: VI
 Creation: 06/10/2022
 Last modification: 10/10/2022
 ----------------------------------------------------------------------------------------------------
@@ -27,16 +27,20 @@ string tipoTrabajo;
 int cantProcesos;
 /* meter esta en el main por la cantidad de Personal int tiempoEjecucion[cantidadPersonal];*/
 int inversionMaquina;
-int cantidadPersonal;
+int cantidadPersonalArea1;
+int cantidadPersonalArea2;
+int cantidadPersonalArea3;
 int salarioPersonal;
 int cantidadOperadores;
 int salarioOperadores;
 int presupuesto;
 int proyeccion;
-int cantPiezas;
-int ensamblajeMaquina;
+int cantPiezasTotales;
+int ensamblajeMaquina = 0;
 int piezasPMin;
 int contador;
+int enEspera = 0;
+int cantPiezasT=0;
 
 // --- Estructura con la informacion de la empresa ---
 struct datosEmpresa
@@ -45,17 +49,13 @@ struct datosEmpresa
     int contador;
 };
 
-// --- Estructura con la informacion de la produccion ---
-struct datosProduccion
-{
-    int cantProcesos = 0;
-    int cantPiezas = 0;
-};
-
 // --- Estructura con la informacion del empleado ---
 struct datosEmpleado
 {
     pthread_t thread;
+    int cantProcesos = 0;
+    int cantPiezas = 0;
+    int contador = 0;
 };
 
 // --- Estructura con la informacion de los costos ---
@@ -73,29 +73,63 @@ void* costoProduccion(void *arg)
     costo *costos;
     costos = (costo*)arg;
     int costoFijoTotal = (costos -> costoFijoEnergia) + (costos -> costoFijoAgua) + (costos-> costoFijoMateriaPrima);
-    int costoVariable = (cantPiezas/piezasPMin) * 2; // Valor fijo
+    int costoVariable = (cantPiezasTotales/piezasPMin) * 2; // Valor fijo
     int costoTotal = costoFijoTotal + costoVariable;
     return NULL;
 }
 
 // --- Metodo produccion por parte del personal humano ---
-void* procesoPersonal(void *piezas)
+void* procesoPersonal(void *args)
 {
-   datosProduccion *produccionStructure = (datosProduccion*) piezas; // *Structure call
+   datosEmpleado *produccionStructure = (datosEmpleado*) args; // *Structure call
     // *Mutex lock*
-    pthread_mutex_lock(&mutex);
-    cout<<"El proceso No."<< contador << ", realizado por el personal humano, ha producido 1 pieza." <<endl;
-    produccionStructure->cantPiezas += 1;
-    // *Mutex Unlock*
-    pthread_mutex_unlock(&mutex);
-    pthread_mutex_destroy(&mutex);
-    return NULL;
+    while(produccionStructure->cantPiezas != cantPiezasT && cantPiezasT > 0)
+    {
+        if (cantPiezasT > 0){
+            pthread_mutex_lock(&mutex);
+            produccionStructure->cantPiezas = produccionStructure->cantPiezas + piezasPMin;
+            cantPiezasT = cantPiezasT - piezasPMin;
+            cout<<"El proceso No."<< produccionStructure->contador << ", realizado por el personal humano, ha producido " << piezasPMin <<  " piezas." << cantPiezasT <<endl;
+            // *Mutex Unlock*
+            pthread_mutex_unlock(&mutex);
+        }
+        else{
+            pthread_exit(NULL);
+        }
+    }
 }
 
 // --- Metodo produccion por parte de la maquina ---
 void* procesoMaquina(void *arg)
 {
+    for (int i = 0; i < cantPiezasTotales; i++){
+        pthread_mutex_lock(&mutex);
+        ensamblajeMaquina++;
+        cout << "La maquina ha ensamblado una pieza" << endl;
+        pthread_mutex_unlock(&mutex);
+    }
+    cout << "Proceso terminado"<< endl;
+    cout << "''''''''''''''''''''\n" << endl;
+    pthread_exit(nullptr);
+}
 
+void* procesoMaquina2(void* arg){
+    pthread_mutex_lock(&mutex);
+    while (ensamblajeMaquina < cantPiezasTotales-2){
+        //pthread_cond_wait(&entregado, &pieza);
+    }
+    pthread_mutex_unlock(&mutex);
+    cout << "\nComenzando el siguiente proceso" << endl;
+    cout << "''''''''''''''''''''" << endl;
+    
+    while (enEspera <= cantPiezasTotales && ensamblajeMaquina > 0){
+        pthread_mutex_lock(&mutex);
+        cout << "Piezas en espera: " << ensamblajeMaquina-- << endl;
+        cout << "La maquina ha ensamblado una pieza" << endl;
+        pthread_mutex_unlock(&mutex);
+        sleep(1);
+    }
+    pthread_exit(nullptr);
 }
 
 // --- Main ---
@@ -111,9 +145,13 @@ int main()
     cout << "Piezas producidas por minuto: ";
     cin >> piezasPMin;
     cout << "Cantidad de piezas totales a producir: ";
-    cin >> cantPiezas;
-    cout << "\nNumero de personas que conforman el personal (sin contar a los operadores): ";
-    cin >> cantidadPersonal;
+    cin >> cantPiezasTotales;
+    cout << "\nNumero de personas que conforman el personal de la 1ra fase (sin contar a los operadores): ";
+    cin >> cantidadPersonalArea1;
+    cout << "\nNumero de personas que conforman el personal de la 2da fase (sin contar a los operadores): ";
+    cin >> cantidadPersonalArea2;
+    cout << "\nNumero de personas que conforman el personal de la 3ra fase (sin contar a los operadores): ";
+    cin >> cantidadPersonalArea3;
     cout << "Numero de personas que conforman al grupo de los operadores: ";
     cin >> cantidadOperadores;
     // Inversion en maquina y personal humano
@@ -126,5 +164,36 @@ int main()
     // Presupuesto
     cout << "\nPresupuesto inicial en dolares ($): ";
     cin >> presupuesto;
-    cout << "Inicializando los procesos...";
+    cout << "Inicializando los procesos...\n";
+    cantPiezasT = cantPiezasTotales;
+    datosEmpleado estructuraDatosEmpleado[cantidadPersonalArea1];
+    for(int i=0; i<cantidadPersonalArea1; i++){
+        estructuraDatosEmpleado[i].cantProcesos = cantPiezasT;
+        estructuraDatosEmpleado[i].contador = i;
+    }
+    for (int k = 0; k < cantidadPersonalArea1; k++){
+            pthread_create(&estructuraDatosEmpleado[k].thread, NULL, procesoPersonal, ( void *)&estructuraDatosEmpleado[k]);
+    }
+    for(int l = 0; l < cantidadPersonalArea1; l++){
+        pthread_join(estructuraDatosEmpleado[l].thread, NULL);
+    }
+    pthread_mutex_init(&mutex, nullptr);
+    pthread_t threads[cantProcesos];
+    
+    for (int i = 0; i < cantProcesos; i++){
+        if (i == 2){
+            pthread_create(&threads[i], nullptr, &procesoMaquina2, nullptr);
+        }
+        else{
+            pthread_create(&threads[i], nullptr, &procesoMaquina, nullptr);
+        }
+    }
+    
+    for(auto thread : threads){
+        pthread_join(thread, nullptr);
+    }
+    
+    cout << "\nCantidad de piezas ensambladas: " << cantPiezasTotales << endl;
+    pthread_exit(NULL);
+    return 0;
 }
