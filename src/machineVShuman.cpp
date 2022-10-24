1de2de8ac5c796426728686df464c0dbbaa56229
Sebastian Juárez <84484212+SebasJuarez@users.noreply.github.com>
2022-10-23 23:09:43 -0600

    Last version of code

--- src/machineVShuman.cpp
+++ src/machineVShuman.cpp
@@ -28,7 +28,9 @@ Last modification: 23/10/2022
 #include <sstream>
 #include <fstream>
 #include <semaphore.h>
+#include <ctime> 
 using namespace std;
+unsigned t0, t1;
 
 // Threads mutex y condicional
 pthread_mutex_t piezas;
@@ -37,8 +39,8 @@ sem_t semaphore;
 
 // Variables a solicitar
 string tipoTrabajo;
-int inversionMaquina;
-int salarioPersonal;
+int inversionMaquina=0;
+int salarioPersonal=0;
 
 // Variables fijas
 int cantProcesos = 1;               //hilos a crear
@@ -50,15 +52,22 @@ int cantidadPersonalArea3 = 1;      //personas en area 3
 int ensambladosArea1 = 0;           //contador para piezas ensambladas en area 1
 int ensambladosArea2 = 0;           //contador para piezas ensambladas en area 2
 int piezasEnsambladas = 0;          //contador para totales piezas ensambladas
+int costoFijoTotal;
+int costoTotal;
+int piezasPMin=2;
+int opcion = 0;
+int opcion2 = 0;
+clock_t tiempoMaquina;
+clock_t tiempoPersonal;
 
 
 // --- Estructura con la informacion de los costos ---
 struct costo{
-    int costoFijoEnergia;
-    int costoFijoAgua;
-    int costoFijoMateriaPrima;
+    int costoFijoEnergia = 10;
+    int costoFijoAgua = 10;
+    int costoFijoMateriaPrima = 10;
     int costoVariable;  
-};
+}cost;
 
 // --- Estructura para cada area de produccion de personal ---
 struct Area1{
@@ -81,10 +90,10 @@ void* costoProduccion(void *arg)
 {
     costo *costos;
     costos = (costo*)arg;
-    int costoFijoTotal = (costos -> costoFijoEnergia) + (costos -> costoFijoAgua) + (costos-> costoFijoMateriaPrima);
-    //int costoVariable = (cantPiezasTotales/piezasPMin) * 2; // Valor fijo
-    //int costoTotal = costoFijoTotal + costoVariable;
-    return NULL;
+    costoFijoTotal = (costos->costoFijoAgua) + (costos->costoFijoEnergia) + (costos->costoFijoMateriaPrima);
+    costos->costoVariable = (cantPiezasTotales/piezasPMin) * 2; // Valor fijo
+    costoTotal = costoFijoTotal + costos->costoVariable;
+    pthread_exit(nullptr);
 }
 
 // --- Proceso area 1 ---
@@ -202,19 +211,19 @@ void *maquina2(void *piezasM)
 int main(){
     cout << "--- B I E N V E N I D O ---" << endl;
     // Informacion general de la empresa
-    //cout << "\nFinalidad de su empresa (fabrica, maquila, entre otros): ";
-    //cin >> tipoTrabajo;
+    cout << "\nFinalidad de su empresa (fabrica, maquila, entre otros): ";
+    cin >> tipoTrabajo;
    
-    // Inversion en maquina y personal humano
-    //cout << "\nSalario del personal en dolares ($): ";
-    //cin >> salarioPersonal;
-    //cout << "Inversion de la maquina en dolares ($): ";
-    //cin >> inversionMaquina;
+    //Inversion en maquina y personal humano
+    cout << "\nSalario del personal en dolares ($): ";
+    cin >> salarioPersonal;
+    cout << "Inversion de la maquina en dolares ($): ";
+    cin >> inversionMaquina;
 
     cout << "Inicializando los procesos...\n\n";
     cout << "--- Produccion por parte del personal humano ---\n\n";
     
-    cout << "\nCantidad de piezas ensambladas: " << piezasEnsambladas << endl;
+    cout << "\nCantidad de piezas a producir: " << cantPiezasTotales << endl;
     pthread_mutex_init(&piezas, nullptr);
     pthread_cond_init(&producidas, nullptr);
     sem_init(&semaphore, 0, 2);
@@ -224,6 +233,7 @@ int main(){
     Area2 arecita[cantidadPersonalArea2];
     Area3 aresota[cantidadPersonalArea3];
     cantidadPiezasPorArea1 = cantPiezasTotales / cantidadPersonalArea1;
+    tiempoPersonal = clock();
     for (int i = 0; i < 1; i++){
         // AREA 1
         for (int i = 0; i < cantidadPersonalArea1; i++){
@@ -270,13 +280,14 @@ int main(){
     }
     
     cout << "\nCantidad de piezas ensambladas: " << piezasEnsambladas << endl;
-    
+    tiempoPersonal = clock() + tiempoPersonal;
     //libera memoria
     sem_destroy(&semaphore);
     pthread_mutex_destroy(&piezas);
     pthread_cond_destroy(&producidas);
 
     cout << "--- Produccion por parte de la maquina ---\n\n";
+    tiempoMaquina = clock();
     double worker;
     bool produccion = false;
     controlMaquina produccionMaquina;
@@ -312,6 +323,48 @@ int main(){
             worker = pthread_create(&tid[2],&attr,maquina2,(void *)&produccionMaquina);
             worker = pthread_join(tid[2],NULL);
         }
+    }
+    tiempoMaquina = clock() + tiempoMaquina;
+    //thread de los costos 
+    costo costo1[1];
+    for (int i = 0; i < 1; i++){
+            pthread_create(&threads[i], nullptr, &costoProduccion, ( void *)&costo1[i]);
+        }
+    int salariofinal = salarioPersonal * 30;
+
+    cout << "\n\n---REPORTE---" << endl;
+    cout << "El salario del personal en el mes seria de: " << salariofinal <<" dolares" << endl;
+    if (salariofinal < inversionMaquina){
+        cout << "El salario del personal es menor a la inversion planificada de la maquina"<< endl;
+        opcion = 1;
+    }
+    else{
+        cout << "El salario del personal mensual es mayor a la inversion de la maquina"<< endl;
+        opcion = 2;
+    }
+    cout << "Los costos fijos se detallan aquí: \nCosto del agua: " << cost.costoFijoAgua << " dolares por litro"<<"\nCosto de la electricidad: " << cost.costoFijoEnergia <<" dolares por kilowatt"<< "\nCosto de la materia prima: " << cost.costoFijoMateriaPrima <<" dolares"<< endl;
+    cout << "El costo total a agregar es de: " << costoTotal << " dolares" << endl;
+    cout << "El tiempo de la produccion del personal es de: " << (float)tiempoPersonal/CLOCKS_PER_SEC << " segundos" << endl;
+    cout << "El tiempo de la produccion de la maquina es de: " << (float)tiempoMaquina/CLOCKS_PER_SEC << " segundos" << endl;
+    if (tiempoMaquina > tiempoPersonal){
+        cout << "El tiempo de la produccion de la maquinaria es mayor a la del tiempo del personal\n" << endl;
+        opcion2 = 1;
+    }
+    else{
+        cout << "El tiempo de la produccion del personal es mayor a la del tiempo de la maquinaria\n"<< endl;
+        opcion2 = 2;
+    }
+    if (opcion == 1 && opcion2 == 1){
+        cout << "Ya que el tiempo de la maquinaria es mayor y el salario del personal es menor\nSe recomienda que no se haga la inversión. Lo mejor seria invertir personal."<< endl;
+    }
+    if (opcion==2 && opcion2==2){
+         cout << "Ya que el salario pensado para el personal es mayor al de la maquina y su tiempo de ejecucion es mayor\nLo mejor seria invertir en maquinaria."<< endl;
+    }
+    if (opcion==1 && opcion2==2){
+         cout << "Ya que el tiempo del personal es mayor y el salario del personal es menor\nDejamos esta decision a su criterio"<< endl;
+    }
+    if (opcion==2 && opcion2==1){
+         cout << "Ya que el salario del personal es mayor y su tiempo de produccion es menor\n Se recomienda utilizar mayor personal y por ende invertir más en ellos"<< endl;
     }
     pthread_attr_destroy(&attr);
     pthread_exit(NULL);
