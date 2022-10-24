7db77cb40205de592dc30e0e175ee846e0be51b3
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-10-23 20:32:56 -0600

    machine methods implemented.

--- src/machineVShuman.cpp
+++ src/machineVShuman.cpp
@@ -4,7 +4,7 @@ Copyright (C), 2022-2023, bl33h, MelissaPerez09, FabianJuarez182, SebasJuarez
 FileName: machineVShuman
 @version: VII
 Creation: 06/10/2022
-Last modification: 22/10/2022
+Last modification: 23/10/2022
 ----------------------------------------------------------------------------------------------------
 * Descripcion: 
 * Establecer la eficiencia y rentabilidad a largo plazo de la produccion de una 
@@ -16,17 +16,23 @@ Last modification: 22/10/2022
 
 
 // Librerias
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <iostream>
+#include <math.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <cstdint>
+#include <sstream>
+#include <fstream>
 #include <semaphore.h>
 using namespace std;
-
 
 // Threads mutex y condicional
 pthread_mutex_t piezas;
 pthread_cond_t producidas;
-sem_t semaphore;
 
 // Variables a solicitar
 string tipoTrabajo;
@@ -37,7 +43,7 @@ int salarioPersonal;
 int cantProcesos = 1;               //hilos a crear
 int cantPiezasTotales = 10;         //piezas totales a producir
 int cantidadPiezasPorArea1 = 0;     //Cantidad de piezas a generar en el area 1
-int cantidadPersonalArea1 = 3;      //personas en area 1
+int cantidadPersonalArea1 = 2;      //personas en area 1
 int cantidadPersonalArea2 = 2;      //personas en area 2
 int cantidadPersonalArea3 = 1;      //personas en area 3
 int ensambladosArea1 = 0;           //contador para piezas ensambladas en area 1
@@ -53,12 +59,6 @@ struct costo{
     int costoVariable;  
 };
 
-struct Area1
-{
-    pthread_t thread;
-    int cantidadPiezasPorArea1a;
-};
-
 // --- Metodo costos ---
 void* costoProduccion(void *arg)
 {
@@ -72,10 +72,9 @@ void* costoProduccion(void *arg)
 
 // --- Proceso area 1 ---
 void* area1(void* arg){
-    Area1 * ws;
-    ws = (Area1 *)arg;
+
     //produccion de piezas en el area 1
-    for (int i = 0; i < ws->cantidadPiezasPorArea1a; i++){
+    for (int i = 0; i < cantPiezasTotales / cantidadPersonalArea1; i++){
         pthread_mutex_lock(&piezas);
         ensambladosArea1++;
         cout << "Persona del AREA 1: ensamblo 1 pieza" << endl;
@@ -126,17 +125,52 @@ void* area3(void* arg){
 
     //produccion de piezas en el area 3
     for (int i = 0; i < ensambladosArea2 / cantidadPersonalArea3; i++){
-        sem_wait(&semaphore);
+        pthread_mutex_lock(&piezas);
         piezasEnsambladas++;
         cout << "Persona del AREA 3: ensamblo 1 pieza" << endl;
         pthread_mutex_unlock(&piezas);
         pthread_cond_signal(&producidas);
         sleep(1);
-        sem_post(&semaphore);
     }
     cout << "\nPersonal del AREA 3 ha terminado de producir"<< endl;
     cout << "''''''''''''''''''''" << endl;
     pthread_exit(nullptr);
+}
+
+// --- Estructura del control de la maquina ---
+struct controlMaquina{
+    int piezasPorLaMaquina = 0;
+    int piezasProceso2 = 0;
+    int piezasProceso3 = 0;
+};
+
+// --- Primer metodo maquina ---
+void *maquina1(void *piezasM)
+{
+    controlMaquina *estructuraMaquina = (controlMaquina*) piezasM; // *Structure call
+    // *Mutex lock*
+    pthread_mutex_lock(&piezas);
+    cout<<"El primer proceso de la maquina ha producido 1 pieza."<<endl;
+    estructuraMaquina->piezasPorLaMaquina += 1;
+    // *Mutex Unlock*
+    pthread_mutex_unlock(&piezas);
+    pthread_mutex_destroy(&piezas);
+    return NULL;
+}
+
+// --- Segundo metodo maquina ---
+void *maquina2(void *piezasM)
+{
+    controlMaquina *estructuraMaquina = (controlMaquina*) piezasM; // *Structure call
+    // *Mutex lock*
+    pthread_mutex_lock(&piezas);
+    cout<<"El segundo proceso de la maquina ha producido 1 pieza."<<endl;
+    estructuraMaquina->piezasProceso2 += 1;
+    cout<<"Piezas totales ensambladas: "<<estructuraMaquina--->piezasProceso2<<endl;
+    // *Mutex Unlock*
+    pthread_mutex_unlock(&piezas);
+    pthread_mutex_destroy(&piezas);
+    return NULL;
 }
 
 
@@ -154,27 +188,18 @@ int main(){
     //cin >> inversionMaquina;
 
     cout << "Inicializando los procesos...\n\n";
-
+    cout << "--- Produccion por parte del personal humano ---\n\n";
+    
     pthread_mutex_init(&piezas, nullptr);
     pthread_cond_init(&producidas, nullptr);
-    sem_init(&semaphore, 0, 2);
     //inicia con la cantidad de procesos que se crean los thread
     pthread_t threads[1];
-    Area1 area[cantidadPersonalArea1];
+    
     cantidadPiezasPorArea1 = cantPiezasTotales / cantidadPersonalArea1;
     for (int i = 0; i < 1; i++){
         for (int i = 0; i < cantidadPersonalArea1; i++){
-            if(i==cantidadPersonalArea1-1){
-                area[i].cantidadPiezasPorArea1a = cantidadPersonalArea1 + cantPiezasTotales%cantidadPersonalArea1;
-            }
-            else{
-                area[i].cantidadPiezasPorArea1a = cantidadPersonalArea1;
-            }
+            pthread_create(&threads[i], nullptr, &area1, nullptr);
         }
-        for (int i = 0; i < cantidadPersonalArea1; i++){
-            pthread_create(&threads[i], nullptr, &area1, ( void *)&area[i]);
-        }
-        
         for (int i = 0; i < cantidadPersonalArea2; i++){
             pthread_create(&threads[i], nullptr, &area2, nullptr);
         }
@@ -188,11 +213,49 @@ int main(){
     }
     
     cout << "\nCantidad de piezas ensambladas: " << piezasEnsambladas << endl;
-    pthread_exit(NULL);
+    //pthread_exit(NULL);
     
-    //libera memoria
-    sem_destroy(&semaphore);
-    pthread_mutex_destroy(&piezas);
-    pthread_cond_destroy(&producidas);
+    cout << "--- Produccion por parte de la maquina ---\n\n";
+    double worker;
+    bool produccion = false;
+    controlMaquina produccionMaquina;
+    pthread_t tid[2];
+    pthread_attr_t attr;
+    cout<<"La produccion por la maquina ha comenzado..."<<endl;
+    while(produccion == false){
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+        worker = pthread_create(&tid[0],&attr,maquina1,(void *)&produccionMaquina);
+        worker = pthread_join(tid[0],NULL);
+        if(produccionMaquina.piezasProceso2 == 10)
+        {
+            produccionMaquina.piezasPorLaMaquina -= 4;
+            produccionMaquina.piezasProceso2 = 10;
+            produccion = true;
+        }
+        if((produccionMaquina.piezasPorLaMaquina % 5 == 0)||(produccionMaquina.piezasPorLaMaquina== 5))
+        {
+            produccionMaquina.piezasPorLaMaquina -= 1;
+            worker = pthread_create(&tid[2],&attr,maquina2,(void *)&produccionMaquina);
+            worker = pthread_join(tid[2],NULL);
+        }
+        worker = pthread_create(&tid[1],&attr,maquina1,(void *)&produccionMaquina);
+        worker = pthread_join(tid[1],NULL);
+        sleep(1);
+        if(produccionMaquina.piezasProceso2 == 10)
+        {
+            produccionMaquina.piezasPorLaMaquina -= 4;
+            produccionMaquina.piezasProceso2 = 10;
+            produccion = true;
+        }
+        if((produccionMaquina.piezasPorLaMaquina % 5 == 0)||(produccionMaquina.piezasPorLaMaquina== 5))
+        {
+            produccionMaquina.piezasPorLaMaquina -=1;
+            worker = pthread_create(&tid[2],&attr,maquina2,(void *)&produccionMaquina);
+            worker = pthread_join(tid[2],NULL);
+        }
+    }
+    pthread_attr_destroy(&attr);
+    pthread_exit(NULL);
     return 0;
 }
